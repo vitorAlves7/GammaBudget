0627126fb8945691d5bc041a01ca74c862e9dbc7
kayrocesar <kayro.cesar.kc@gmail.com>
2024-05-15 14:49:54 -0300

    feat:  add componente maiores despesas

---
+++ src/app/components/top-expenses/top-expenses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-expenses',
+  standalone: true,
+  imports: [],
+  templateUrl: './top-expenses.component.html',
+  styleUrl: './top-expenses.component.scss'
+})
+export class TopExpensesComponent {
+
+}
