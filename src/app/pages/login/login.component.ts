1eaf07722c7da14cc6756043678ed8573b4d5019
vitorAlves7 <vitor.alves07750@outlook.com>
2024-05-14 22:02:18 -0300

    feat: adding login service ngModel

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-
+import { NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -11,11 +12,16 @@ import { CommonModule } from '@angular/common';
     CommonModule,
     RouterOutlet,
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    FormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  onSubmit(userLoginData: NgForm){
+    console.log(userLoginData.value);
+  }
 
 }
