610c58d4a91ebeab0aa052de2933bba8ddcbda83
kayrocesar <kayro.cesar.kc@gmail.com>
2024-05-16 22:53:09 -0300

    feat: add componentes de gráfico na home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,13 +2,22 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { TopExpensesComponent } from '../../components/top-expenses/top-expenses.component';
 import { TotalExpensesComponent } from '../../components/total-expenses/total-expenses.component';
+import { HomeChartComponent } from '../../components/home-chart/home-chart.component';
+import { UpcomingExpensesComponent } from '../../components/upcoming-expenses/upcoming-expenses.component';
 
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent,TopExpensesComponent,TotalExpensesComponent],
+  imports:
+    [
+      NavbarComponent,
+      TopExpensesComponent,
+      TotalExpensesComponent,
+      HomeChartComponent,
+      UpcomingExpensesComponent
+    ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
