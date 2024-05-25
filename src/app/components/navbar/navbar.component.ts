0d87e53d7e554549deb9d605be41334164838e8e
kayrocesar <kayro.cesar.kc@gmail.com>
2024-05-25 14:45:09 -0300

    feat: add navegação entre telas por rota

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+
+  currentRoute!: string;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      this.currentRoute = this.router.url;
+    });
+  }
+
+  navigateTo(route: string): void {
+    this.router.navigate([route]);
+  }
 
 }
