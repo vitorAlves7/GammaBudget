359ff61a665ed71c2d96c92f88e22b0c6be626ef
kayrocesar <kayro.cesar.kc@gmail.com>
2024-05-29 00:55:14 -0300

    fix: correções na tela de lançamentos

--- src/app/pages/launches/launches.component.ts
+++ src/app/pages/launches/launches.component.ts
@@ -7,34 +7,53 @@ import { ItemFilterPipe } from './item-filter.pipe';
 import { OptionFilterPipe } from './option-filter.pipe';
 import { MonthSelectorComponent } from '../../components/month-selector/month-selector.component';
 import { MonthYearFilterPipe } from "../../components/month-selector/month-year-filter/month-year-filter.pipe";
+import { ApiService } from '../../services/api-service.service';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
-interface Item {
+
+
+
+interface Incoming {
   id?: number;
-  descricao: string;
-  categoria: string;
-  data: string;
-  valor: number;
-  status?: any;
+  name: string;
+  description: string;
+  amount: number;
+  launch_date: string;
+  category: string;
 }
 
+interface Expense {
+  id?: number;
+  name: string;
+  description: string;
+  amount: number;
+  expiration_date: string;
+  paid: boolean;
+  payment_date: string;
+  category: string;
+}
+
+
+
 @Component({
-    selector: 'app-launches',
-    standalone: true,
-    templateUrl: './launches.component.html',
-    styleUrl: './launches.component.scss',
-    imports: 
-     [
-       NavbarComponent,
-       CommonModule, 
-       FormsModule, 
-       RouterModule, 
-       ItemFilterPipe, 
-       OptionFilterPipe, 
-       MonthSelectorComponent, 
-       MonthYearFilterPipe
-      ]
+  selector: 'app-launches',
+  standalone: true,
+  templateUrl: './launches.component.html',
+  styleUrl: './launches.component.scss',
+  imports:
+    [
+      NavbarComponent,
+      CommonModule,
+      FormsModule,
+      RouterModule,
+      ItemFilterPipe,
+      OptionFilterPipe,
+      MonthSelectorComponent,
+      MonthYearFilterPipe,
+      HttpClientModule
+    ]
 })
 export class LaunchesComponent {
   showModal = false;
@@ -42,32 +61,52 @@ export class LaunchesComponent {
   showAddModal = false;
   selectedItem: any;
   editItem: any;
-  newItem: any = {
-    descricao: '',
-    categoria: '',
-    data: '',
-    valor: null,
-    status: '',
-  };
-
   itemType: string | undefined;
   saldoPrevisto = 0;
 
-  items: Item[] = [
-    // { descricao: 'Item 1', categoria: 'Categoria 1', data: '2024-05-19', valor: 100 },
-    // { descricao: 'Item 2', categoria: 'Categoria 2', data: '2024-05-20', valor: 200 },
-    // // Outros itens...
-  ];
+  items: any[]=[];
+  incomingList: Incoming[] = [];
+  expenses: Expense[] = [];
+
+  incoming: Incoming = {
+    name: '',
+    description: '',
+    amount: 0,
+    launch_date: '',
+    category: '',
+  };
+
+  expense: Expense = {
+    name: '',
+    description: '',
+    amount: 0,
+    expiration_date: '',
+    paid: false,
+    payment_date: '',
+    category: '',
+  }
+
+
+
+
 
   filterText: string = '';
-  selectedFilterOption: string= '';
+  selectedFilterOption: string = '';
 
   selectedMonth: number = new Date().getMonth();
-  selectedYear: number = new Date().getFullYear(); 
+  selectedYear: number = new Date().getFullYear();
 
 
   openModal(item: any) {
+
     this.selectedItem = item;
+    console.log(this.selectedItem)
+
+    if (this.selectedItem.amount > 0) {
+      this.itemType = 'incoming'
+    } else {
+      this.itemType = 'expense'
+    }
     this.showModal = true;
   }
 
@@ -78,6 +117,7 @@ export class LaunchesComponent {
   openEditModal() {
     this.editItem = { ...this.selectedItem };
     this.showEditModal = true;
+    this.showModal = false;
   }
 
   closeEditModal() {
@@ -90,13 +130,22 @@ export class LaunchesComponent {
 
   updateItem() {
     
-    if(this.selectedItem.valor< 0){
-      this.editItem.valor = -Math.abs(this.editItem.valor);   
+    if (this.selectedItem.amount < 0) {
+      this.editItem.amount = -Math.abs(this.editItem.amount);
+      Object.assign(this.selectedItem, this.editItem);
+      this.updateExpenseItem(this.selectedItem.id, this.selectedItem);
+      
+    }else{
+      Object.assign(this.selectedItem, this.editItem);
+      this.updateIncomingListItem(this.selectedItem.id, this.selectedItem);
+
     }
 
     Object.assign(this.selectedItem, this.editItem);
+    
     this.calculateSaldoPrevisto();
     this.closeEditModal();
+    console.log(this.selectedItem.id)
   }
 
   deleteItem() {
@@ -104,24 +153,54 @@ export class LaunchesComponent {
     if (index > -1) {
       this.items.splice(index, 1);
     }
+    console.log('Estou no delete');
+    console.log(this.selectedItem.id);
+    if(this.selectedItem.amount > 0){
+      this.deleteIncomingListItem(this.selectedItem.id);
+    }else{
+      this.deleteExpenseItem(this.selectedItem.id);
+    }
+    
     this.calculateSaldoPrevisto();
     this.closeModal();
   }
+
   deleteAllItems() {
     this.items = [];
+    
     this.calculateSaldoPrevisto();
   }
 
   openAddModal(type: string) {
-    this.newItem = {
-      descricao: '',
-      categoria: '',
-      data: '',
-      valor: null,
-    };
+
+    this.resetInputs(type)
+
     this.itemType = type;
     this.calculateSaldoPrevisto();
     this.showAddModal = true;
+
+  }
+
+  resetInputs(type:string){
+     {
+      this.incoming = {
+        name: '',
+        description: '',
+        amount: 0,
+        launch_date: '',
+        category: '',
+      };
+  
+      this.expense = {
+        name: '',
+        amount: 0,
+        expiration_date: '',
+        paid: false,
+        payment_date: '',
+        category: '',
+        description: '',
+      }
+    }
   }
 
   closeAddModal() {
@@ -129,59 +208,134 @@ export class LaunchesComponent {
   }
 
   addItem() {
-    if (this.itemType === 'despesa') {
-      this.newItem.valor = -Math.abs(this.newItem.valor);
+    console.log(this.itemType)
+    if (this.itemType === 'expense') {
+      this.expense.amount = -Math.abs(this.expense.amount);
+      console.log()
+      this.items.push({ ...this.expense });
+      this.addItemToExpenses(this.expense)
     }
-    this.items.push({ ...this.newItem });
+    else {
+      this.items.push({ ...this.incoming });
+      this.addItemToIncomingList(this.incoming)
+    }
+
+
     this.calculateSaldoPrevisto();
     this.closeAddModal();
-   
+    console.log(this.items)
+
   }
   private calculateSaldoPrevisto() {
-    const mesAtual = new Date().getMonth() + 1;
-    const anoAtual = new Date().getFullYear();
-
-    this.saldoPrevisto = this.items
-      .filter((item) => {
-        const itemDate = new Date(item.data + 'T00:00:00');
-        const isSameYear = itemDate.getFullYear() === anoAtual;
-        const isSameMonth = itemDate.getMonth() + 1 === mesAtual;
-        const isRevenue = item.valor > 0;
-        const isUnpaidExpense = item.valor < 0 && item.status === 'Não Pago';
-
-        return isSameYear && isSameMonth && (isRevenue || isUnpaidExpense);
-      })
-      .reduce((saldo, item) => saldo + item.valor, 0);
-  }
-
-  ngOnInit(): void {
-    this.calculateSaldoPrevisto();
-  }
-  constructor() {
-
-
+    this.saldoPrevisto = 0;
   
-    this.selectedMonth = new Date().getMonth();
-    this.selectedYear = new Date().getFullYear();
+    // Filtrar receitas (Incoming) do mês e ano selecionados
+    const filteredIncomings = this.items.filter(item => {
+      if ('launch_date' in item) {
+        const itemDate = new Date(item.launch_date);
+        const isSameYear = itemDate.getFullYear() === this.selectedYear;
+        const isSameMonth = itemDate.getMonth()  === this.selectedMonth;
+        return isSameYear && isSameMonth;
+      }
+      return false;
+    }) as Incoming[];
 
-    console.log(this.selectedMonth)
+    console.log(filteredIncomings)
     console.log(this.selectedYear)
-    console.log("----------------")
+    console.log(this.selectedMonth)
+  
+    
+    const saldoPrevistoReceitas = filteredIncomings.reduce((saldo, incoming) => saldo + incoming.amount, 0);
+  
+    const filteredExpenses = this.items.filter(item => {
+      if ('expiration_date' in item && !item.paid) {
+        const itemDate = new Date(item.expiration_date + 'T00:00:00');
+        const isSameYear = itemDate.getFullYear() === this.selectedYear ;
+        const isSameMonth = itemDate.getMonth()  === this.selectedMonth;
+        return isSameYear && isSameMonth;
+      }
+      return false;
+    }) as Expense[];
 
+    console.log(filteredExpenses)
+  
+   
+    const saldoPrevistoDespesas = filteredExpenses.reduce((saldo, expense) => saldo + expense.amount, 0);
+  
+    
+    this.saldoPrevisto = saldoPrevistoReceitas + saldoPrevistoDespesas;
+  
+    console.log('Saldo Previsto:', this.saldoPrevisto);
+  }
+  ngOnInit(): void {
+    this.getData(); 
+  }
+  
+  constructor(private apiService: ApiService) {}
+  
+  getData() {
+    this.apiService.getData('incomingList')
+      .subscribe(response => {
+        this.incomingList = response;
+        this.combineLists();
+      });
+
+    this.apiService.getData('expenses')
+      .subscribe(response => {
+        this.expenses = response;
+        this.combineLists();
+      });
+  }
+  
+  combineLists(): void {
+    if (this.incomingList && this.expenses) { 
+      this.items = [...this.incomingList, ...this.expenses];
+      this.calculateSaldoPrevisto(); 
+    }
+  }
+  
+  addItemToIncomingList(item: any): void {
+    this.apiService.postData('incomingList', item).subscribe(() => {
+      this.getData(); 
+    });
   }
 
+  updateIncomingListItem(id: number, item: any): void {
+    this.apiService.updateData('incomingList', id, item).subscribe(() => {
+      this.getData(); 
+    });
+  }
+
+  deleteIncomingListItem(id: number): void {
+    this.apiService.deleteData('incomingList', id).subscribe(() => {
+      this.getData();
+    });
+  }
+
+  addItemToExpenses(item: any): void {
+    this.apiService.postData('expenses', item).subscribe(() => {
+      this.getData(); 
+    });
+  }
+
+  updateExpenseItem(id: number, item: any): void {
+    this.apiService.updateData('expenses', id, item).subscribe(() => {
+      this.getData(); 
+    });
+  }
+
+  deleteExpenseItem(id: number): void {
+    this.apiService.deleteData('expenses', id).subscribe(() => {
+      this.getData(); 
+    });
+  }
 
   onMonthYearChanged(event: { month: number, year: number }) {
     this.selectedMonth = event.month;
     this.selectedYear = event.year;
-
-    console.log("--------MUDOU--------")
-    console.log(this.selectedMonth)
-    console.log(this.selectedYear)
-    
-
-    
+    this.calculateSaldoPrevisto();
   }
 
-  
+
+
 }
