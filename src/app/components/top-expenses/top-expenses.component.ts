5859d0c09351622a4b546c664d5b69427904d382
kayrocesar <kayro.cesar.kc@gmail.com.br>
2024-05-30 19:33:28 -0300

    feat: utiliza serviço para obter os dados do grafico de maiores despesas

--- src/app/components/top-expenses/top-expenses.component.ts
+++ src/app/components/top-expenses/top-expenses.component.ts
@@ -1,19 +1,25 @@
 import { CommonModule } from '@angular/common';
-import { Component, ViewChild, OnInit } from '@angular/core';
-import { ChartComponent, NgApexchartsModule } from 'ng-apexcharts';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ExpensesService } from '../../services/expenses/expenses.service';
+import { Expense } from '../../types/expense-type';
+import { ChartComponent, NgApexchartsModule} from 'ng-apexcharts';
+
+
 
 export type ChartOptions = {
-  series: any,
-  chart: any,
-  xaxis: any,
-  title?: any,
-  plotOptions?: any,
-  fill?: any,
-  legend?: any,
-  dataLabels?: any,
-  tooltip?: any,
-  grid?: any,
-  yaxis?: any,
+  series: any;
+  chart: any;
+  xaxis: any;
+  title?: any;
+  plotOptions?: any;
+  fill?: any;
+  legend?: any;
+  dataLabels?: any;
+  tooltip?: any;
+  grid?: any;
+  yaxis?: any;
+  stroke?: any;
+  states?: any;
 };
 
 @Component({
@@ -23,94 +29,127 @@ export type ChartOptions = {
   templateUrl: './top-expenses.component.html',
   styleUrls: ['./top-expenses.component.scss']
 })
-export class TopExpensesComponent implements OnInit {
+export class TopExpensesComponent implements OnInit{
 
-  @ViewChild("chart") chart: ChartComponent | undefined;
-  public chartOptions: Partial<ChartOptions> = {};
+  
+  public chartOptions: Partial<ChartOptions> | any;
 
-  constructor() { }
 
-  ngOnInit(): void {
-    this.chartOptions = {
-      series: [
-        {
-          name: "Despesas",
-          data: [1000, 600, 520, 141.50],
-          color: "#F05252"
-        }
-      ],
-      chart: {
-        type: "bar",
-        width: "100%",
-        height: 500,
-        toolbar: {
-          show: true,
-        }
-      },
-      fill: {
-        opacity: 1,
-      },
-      plotOptions: {
-        bar: {
-          horizontal: true,
-          columnWidth: "100%",
-          borderRadiusApplication: "end",
-          borderRadius: 6,
-          dataLabels: {
-            position: "top",
+  expenses: Expense[] = [];
+
+  constructor(private expenseService: ExpensesService) {
+
+
+    this.expenseService.getExpenses().subscribe(
+      (response: Expense[]) => {
+        this.expenses = this.filterTopExpenses(response);
+    
+      
+      this.chartOptions = {
+        series: [
+          {
+            name: "Despesas",
+            data: this.expenses.map(expense => expense.amount),
+            color: "#F05252"
+          }
+        ],
+        chart: {
+          type: "bar",
+          width: "100%",
+          height: 500,
+          toolbar: {
+            show: true,
+          }
+        },
+        fill: {
+          opacity: 1,
+        },
+        plotOptions: {
+          bar: {
+            horizontal: true,
+            columnWidth: "100%",
+            borderRadiusApplication: "end",
+            borderRadius: 6,
+            dataLabels: {
+              position: "top",
+            },
           },
         },
-      },
-      legend: {
-        show: true,
-        position: "bottom",
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      tooltip: {
-        shared: true,
-        intersect: false,
-        y: {
-          formatter: function (val: number) {
-            return val.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-          }
-        }
-      },
-      xaxis: {
-        labels: {
+        legend: {
           show: true,
-          style: {
-            fontFamily: "Roboto, Open-sans",
-            cssClass: 'text-xs font-normal fill-gray-500'
+          position: "bottom",
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        tooltip: {
+          shared: true,
+          intersect: false,
+          y: {
+            formatter: function (val: number) {
+              return val.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+            }
+          }
+        },
+        xaxis: {
+          labels: {
+            show: true,
+            style: {
+              
+              cssClass: 'text-xs font-normal fill-gray-500'
+            },
+          },
+          categories: this.expenses.map(expense => expense.name),
+          axisTicks: {
+            show: true,
+          },
+          axisBorder: {
+            show: true,
           },
         },
-        categories: ["Agua", "Luz", "Comida", "Ração"],
-        axisTicks: {
-          show: true,
-        },
-        axisBorder: {
-          show: true,
-        },
-      },
-      yaxis: {
-        labels: {
-          show: true,
-          style: {
-            fontFamily: "Roboto, Open-sans",
-            cssClass: 'text-xs font-normal fill-gray-500'
+        yaxis: {
+          labels: {
+            show: true,
+            style: {
+              
+              cssClass: 'text-xs font-normal fill-gray-500'
+            }
           }
-        }
-      },
-      grid: {
-        show: true,
-        strokeDashArray: 4,
-        padding: {
-          left: 3,
-          right: 2,
-          top: -20
         },
-      },
-    };
+        grid: {
+          show: false,
+          strokeDashArray: 4,
+          padding: {
+            left: 10,
+            right: 2,
+            top: -20
+          },
+        },
+      };
+    },
+    );
+
+
+   
   }
+  ngOnInit(): void {
+    
+  }
+
+
+
+
+ 
+    
+  
+  
+  filterTopExpenses(expenses: Expense[]): Expense[] {
+    expenses.forEach(expense => {
+      expense.amount = Math.abs(expense.amount);
+    });
+
+    expenses.sort((a, b) => b.amount - a.amount);
+    return expenses.slice(0, 4);
+  }
+
 }
