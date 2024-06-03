27bf42f46aa88963a01d0e3821563c38cfda6e59
kayrocesar <kayro.cesar.kc@gmail.com.br>
2024-06-02 23:16:26 -0300

    feat: criação do serviço de receitas

--- src/app/services/incomings/incomings.service.ts
+++ src/app/services/incomings/incomings.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Incoming } from '../../types/incoming-type';
 import { Observable } from 'rxjs';
 
@@ -9,11 +9,35 @@ import { Observable } from 'rxjs';
 })
 export class IncomingsService {
 
-  private apiUrl: string = `${environment.apiBaseUrl}/incomings`; 
+  private apiUrl: string = `${environment.apiBaseUrl}`;
 
   constructor(private http: HttpClient) {}
 
-  getIncomings(): Observable<Incoming[]> {
-    return this.http.get<Incoming[]>(this.apiUrl);
+  getIncomingList(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${environment.apiEndpoints.incomingList}/${environment.userId}/`,this.getHttpOptions());
   }
+
+  getIncomingDetail(userId: number, id: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${environment.apiEndpoints.incomingDetail}/${userId}/${id}/`,this.getHttpOptions());
+  }
+
+  addItemToIncomingList(item: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/${environment.apiEndpoints.addItemToIncomings}/${environment.userId}/`, item,this.getHttpOptions());
+  }
+
+  updateIncomingListItem(id: string, item: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${environment.apiEndpoints.updateIncomingItem}/${environment.userId}/${id}/`, item,this.getHttpOptions());
+  }
+
+  deleteIncomingListItem(id: string): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/${environment.apiEndpoints.deleteIncomingItem}/${environment.userId}/${id}/`,this.getHttpOptions());
+  }
+
+
+  private getHttpOptions() {
+    return {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+  }
+
 }
