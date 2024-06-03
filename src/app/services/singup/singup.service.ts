537744fccb5af0de738151e66bbffcaf0c70fd10
Vitor Alves <vitor.alves07750@outlook.com>
2024-06-03 17:46:59 -0300

    feat: adding connection with api and redirect to login page if singup sucess

--- src/app/services/singup/singup.service.ts
+++ src/app/services/singup/singup.service.ts
@@ -1,27 +1,26 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment'
+import { map } from 'rxjs/operators';
 
 
 @Injectable({
   providedIn: "root"
 })
-export class AuthService {
+export class SingUpService {
 
 	constructor(private _http: HttpClient) { }
 
 
 	createUser(useremail: string, password: string) {
-
-        const response = this._http
-        .post(`${ environment.apiUrl }user/register`,
+        return this._http
+        .post<any>(`${ environment.apiUrl }user/register`,
             {
                 email: useremail,
                 password: password,
             }
-        );
-        console.log(response);
-        return response
+        ).pipe(map(response =>
+            response
+        ))
     }
-
 }
