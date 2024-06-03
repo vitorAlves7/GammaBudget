dc08e140d9147dc6e964ec965fa221e68d69c21c
kayrocesar <kayro.cesar.kc@gmail.com.br>
2024-06-02 23:17:04 -0300

    feat: criação do serviço de despesas

--- src/app/services/expenses/expenses.service.ts
+++ src/app/services/expenses/expenses.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
@@ -9,13 +9,37 @@ import { Expense } from '../../types/expense-type';
 })
 export class ExpensesService {
 
-  private apiUrl: string = `${environment.apiBaseUrl}/expenses`; 
+  private apiUrl: string = `${environment.apiBaseUrl}`;
+  headers = new HttpHeaders({ 'Content-Type': 'application/json' });
 
   constructor(private http: HttpClient) {}
 
-  getExpenses(): Observable<Expense[]> {
-    return this.http.get<Expense[]>(this.apiUrl);
+  getExpensesList(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${environment.apiEndpoints.expensesList}/${environment.userId}/`,this.getHttpOptions());
   }
+
+  getExpenseDetail(id: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${environment.apiEndpoints.expenseDetail}/${environment.userId}/${id}/`,this.getHttpOptions());
+  }
+
+  addItemToExpenses( item: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/${environment.apiEndpoints.addItemToExpenses}/${environment.userId}/`, item,this.getHttpOptions());
+  }
+
+  updateExpenseItem( id: string, item: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${environment.apiEndpoints.updateExpenseItem}/${environment.userId}/${id}/`, item,this.getHttpOptions());
+  }
+
+  deleteExpenseItem(id: string): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/${environment.apiEndpoints.deleteExpenseItem}/${environment.userId}/${id}/`,this.getHttpOptions());
+  }
+
+  private getHttpOptions() {
+    return {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+  }
+
 
 }
 
