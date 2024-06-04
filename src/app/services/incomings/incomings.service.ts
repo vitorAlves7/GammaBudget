f271d79f6d2cd46c939898029310654f680e8253
Vitor Alves <vitor.alves07750@outlook.com>
2024-06-04 00:26:45 -0300

    fix: adding pro variables

--- src/app/services/incomings/incomings.service.ts
+++ src/app/services/incomings/incomings.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../../environments/environment.prod';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Incoming } from '../../types/incoming-type';
 import { Observable } from 'rxjs';
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,24 @@
 export const environment = {
-    production: true
+    production: true,
+    apiUrl: 'https://back-end-d5im.onrender.com/',
+    clientId: 'BOZnI3UaXDvRFHa3tYE4EN7BIywhOZoDLofZ8zsR',
+    clientSecret: 'ikUbBWILHVPVv7flR3zFYcKJMowapvsEUxN9VxiB50Bcz5BCifNAwGrxvwNIf8wLaFebzkZ9TX3nKBw5qcR9byMfiOxtQLaSkGHwlQ75SmxccWuW9sFayDiNGZVW3T5p',
+    grantType: 'password',
+    apiBaseUrl: 'https://back-end-d5im.onrender.com/',
+    apiEndpoints: {
+      incomingList: 'budget/v1/incoming/list',
+      incomingDetail: 'budget/v1/incoming/detail',
+      addItemToIncomings: 'budget/v1/incoming/create',
+      updateIncomingItem: 'budget/v1/incoming/update',
+      deleteIncomingItem: 'budget/v1/incoming/delete',
+      expensesList: 'budget/v1/revenue/list',
+      expenseDetail: 'budget/v1/revenue/detail',
+      addItemToExpenses: 'budget/v1/revenue/create',
+      updateExpenseItem: 'budget/v1/revenue/update',
+      deleteExpenseItem: 'budget/v1/revenue/delete',
+      listCategoriesExpense: 'budget/v1/revenue/list-categories',
+      listCategoriesIncoming: 'budget/v1/incoming/list-categories'
+
+    },
+    userId: '0'
   };
