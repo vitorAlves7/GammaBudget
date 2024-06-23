a5977399886caa1c2c2d60fba1e6723ebbcfa31c
kayrocesar <kayro.cesar.kc@gmail.com>
2024-06-23 20:38:34 -0300

    fix: corrige import httclient dos testes

--- src/app/services/category/expense-category.service.spec.ts
+++ src/app/services/category/expense-category.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ExpenseCategoryService } from './expense-category.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ExpenseCategoryService', () => {
   let service: ExpenseCategoryService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], 
+      providers: [ExpenseCategoryService]
+    });
     service = TestBed.inject(ExpenseCategoryService);
   });
 
--- src/app/services/incomings/incomings.service.spec.ts
+++ src/app/services/incomings/incomings.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { IncomingsService } from './incomings.service';
 
@@ -6,7 +7,10 @@ describe('IncomingsService', () => {
   let service: IncomingsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], // Adicione HttpClientTestingModule aqui
+      providers: [IncomingsService] // Fornece o IncomingsService explicitamente
+    });
     service = TestBed.inject(IncomingsService);
   });
 
