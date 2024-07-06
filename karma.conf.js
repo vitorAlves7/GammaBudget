4b0a7acfd0ccaa6d975db15e6a61e82ac14d02c7
Kayro César <kayro.cesar.kc@gmail.com>
2024-07-06 15:37:02 +0000

    Update 2 files
    - /.gitlab-ci.yml
    - /karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,13 +1,18 @@
+const webpackConfig = require('./webpack.config');  // Ajuste o caminho para seu arquivo de configuração do webpack
+
 module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine'],
     files: [
+      'src/app/**/*.spec.ts'
     ],
     exclude: [
     ],
     preprocessors: {
+      'src/app/**/*.spec.ts': ['webpack']
     },
+    webpack: webpackConfig,
     reporters: ['progress', 'junit'],
     junitReporter: {
       outputDir: 'tests/reports',
@@ -23,8 +28,8 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless', 'FirefoxHeadless'],  // Updated to remove Safari
+    browsers: ['FirefoxHeadless'],
     singleRun: true,
     concurrency: Infinity
-  })
-}
+  });
+};
