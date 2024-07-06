8f1b601d8d65bfeb7ad323ee998aa51d266c9381
Kayro César <kayro.cesar.kc@gmail.com>
2024-07-06 17:04:17 +0000

    Update 3 files
    - /webpack.config.js
    - /karma.conf.js
    - /.gitlab-ci.yml

--- karma.conf.js
+++ karma.conf.js
@@ -1,18 +1,22 @@
 module.exports = function(config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine',"karma-typescript"],
+    frameworks: ['jasmine', 'karma-typescript'],
     files: [
-      'src/**/*.spec.ts'
+      'src/**/*.spec.ts'  // Arquivos de teste TypeScript
     ],
     exclude: [],
     preprocessors: {
-      "**/*.spec.ts": "karma-typescript" 
-  },
-    reporters: ['progress', 'junit' ,"karma-typescript" ],
+      'src/**/*.spec.ts': ['karma-typescript']  // Usando karma-typescript como preprocessor
+    },
+    reporters: ['progress', 'junit', 'karma-typescript'],
     junitReporter: {
       outputDir: 'tests/reports',
       useBrowserName: true
+    },
+    karmaTypescriptConfig: {
+      tsconfig: './tsconfig.json'  // Caminho para o seu arquivo tsconfig.json
+      // Outras opções de configuração do karma-typescript, se necessário
     },
     port: 9876,
     colors: true,
