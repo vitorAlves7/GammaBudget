d467cd6a1e2eb659eda62c2c277c1440b271bcc0
Vitor Alves <vitor.alves07750@outlook.com>
2024-07-08 13:46:01 -0300

    feat:create daily-chart components

---
+++ src/app/src/app/components/daily-chart/daily-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-daily-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './daily-chart.component.html',
+  styleUrl: './daily-chart.component.scss'
+})
+export class DailyChartComponent {
+
+}
