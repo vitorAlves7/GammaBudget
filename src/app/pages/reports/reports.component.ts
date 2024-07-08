608bc3511523347b215f92efd7771f896997559d
Vitor Alves <vitor.alves07750@outlook.com>
2024-07-08 02:09:41 -0300

    feat: adding app-month-selector in reports component

--- src/app/pages/reports/reports.component.ts
+++ src/app/pages/reports/reports.component.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { MonthYearFilterPipe } from '../../components/month-selector/month-year-filter/month-year-filter.pipe';
+import { MonthSelectorComponent } from '../../components/month-selector/month-selector.component';
+
 
 @Component({
   selector: 'reports',
   standalone: true,
   imports: [
-    NavbarComponent
+    NavbarComponent,
+    MonthSelectorComponent,
+    MonthYearFilterPipe,
   ],
   templateUrl: './reports.component.html',
   styleUrl: './reports.component.scss'
 })
 export class ReportsComponent {
+  selectedMonth: number = new Date().getMonth();
+  selectedYear: number = new Date().getFullYear();
+
+  limit_date: string = '';
+
+  onMonthYearChanged(event: { month: number; year: number }) {
+    this.selectedMonth = event.month;
+    this.selectedYear = event.year;
+    this.limit_date = `${this.selectedYear}-${(this.selectedMonth+1).toString().padStart(2, '0')}-${'10'}`;
+  }
 
 }
